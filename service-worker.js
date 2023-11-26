683185accd91184c200dee7fa04f049bb4ece90e
Gulshan Kumar <74947700+gulshank99@users.noreply.github.com>
2023-11-26 10:24:39 +0530

    PWA done

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,30 @@
-// const cacheName = 'your-pwa-cache-v1';
+const cacheName = 'your-pwa-cache-v1';
 
-// self.addEventListener('install', (event) => {
-//     event.waitUntil(
-//         caches.open(cacheName).then((cache) => {
-//             return cache.addAll([
-//                 '/',
-//                 '/index.html',
-//                 '/manifest.json',
-//                 '/Assets',
-//                 '/app.js',
-//                 '/styles.css' 
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/manifest.json',
+                '/Assets',
+                '/app.js',
+                '/styles.css',
+                '/register.html',
+                '/register.css',
+                '/register.js',
+                 
 
-//             ]);
-//         })
-//     );
-// });
 
-// self.addEventListener('fetch', (event) => {
-//     event.respondWith(
-//         caches.match(event.request).then((response) => {
-//             return response || fetch(event.request);
-//         })
-//     );
-// });
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
